2b56c89e9310236bdd79d6384f58625380386a9c
Thirukumaran Vaseeharan <haran@metronome.com>
2025-01-19 23:40:32 -0500

    Remove devenv

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" ];
+      perSystem = { config, system, ... }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.fenix.overlays.default
+            ];
+          };
+          rust-toolchain = pkgs.fenix.stable.toolchain;
+          rust-platform = pkgs.makeRustPlatform {
+            cargo = rust-toolchain;
+            rustc = rust-toolchain;
+          };
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+          devShells.default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              rust-toolchain
+              cargo-watch
+            ] ++ lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
+            shellHook = ''
+              export RUST_LOG="info"
+              export RUST_BACKTRACE=1
+            '';
+          };
+        };
+    };
+}
